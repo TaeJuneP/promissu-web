d552129891640d97309041b2114bb45a43b5cae0
taejune park <ptj1554@gmail.com>
2019-02-06 02:36:21 +0900

    insert url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
 import iosButtonImg from "./button_download_ios.png";
 import androidButtonImg from "./button_download_android.png";
 class App extends Component {
@@ -10,8 +10,20 @@ class App extends Component {
         <div className="main_container">
           <img src={logo} className="App-logo" alt="logo" />
           <div className="buttoncontain">
-          <img src={androidButtonImg} className="iosbutton"  alt="Ios" />
-          <img src={iosButtonImg} className="iosbutton"  alt="Ios" />
+            <a
+              href={
+                "https://play.google.com/store/apps/details?id=com.simsimhan.promissu&hl=ko"
+              }
+            >
+              <img src={androidButtonImg} className="iosbutton" alt="Ios" />
+            </a>
+            <a
+              href={
+                "https://play.google.com/store/apps/details?id=com.simsimhan.promissu&hl=ko"
+              }
+            >
+              <img src={iosButtonImg} className="iosbutton" alt="Ios" />
+            </a>
           </div>
         </div>
       </div>
