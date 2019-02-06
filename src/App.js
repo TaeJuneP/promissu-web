374834448728294719d1318aaffcc61cd9849039
TaeJuneP <35757581+TaeJuneP@users.noreply.github.com>
2019-02-06 22:45:22 +0900

    start design

--- src/App.js
+++ src/App.js
@@ -3,28 +3,25 @@ import logo from "./logo.png";
 import "./App.css";
 import iosButtonImg from "./button_download_ios.png";
 import androidButtonImg from "./button_download_android.png";
+const androidUrl =   "https://play.google.com/store/apps/details?id=com.simsimhan.promissu&hl=ko"
 class App extends Component {
   render() {
     return (
       <div className="mainpage">
+        <img src={logo} className="App-logo" alt="logo" />
         <div className="main_container">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="mainh3">지각쟁이들</h1>
+          <h1 className="mainh3">안돼요안돼</h1>
+          <h4 className="mainh4">약속은 약속이다.</h4>
           <div className="buttoncontain">
-            <a
-              href={
-                "https://play.google.com/store/apps/details?id=com.simsimhan.promissu&hl=ko"
-              }
-            >
+            <a href={androidUrl}>
               <img src={androidButtonImg} className="iosbutton" alt="Ios" />
             </a>
-            <a
-              href={
-                "https://play.google.com/store/apps/details?id=com.simsimhan.promissu&hl=ko"
-              }
-            >
+              <a href={androidUrl}>
               <img src={iosButtonImg} className="iosbutton" alt="Ios" />
             </a>
           </div>
+          <img className="main_image" alt='image1'/>
         </div>
       </div>
     );
