683a56626756bef78a17ce3bf712bc5fb37e1529
taejune park <ptj1554@gmail.com>
2019-02-08 19:50:22 +0900

    Updates

---
+++ precache-manifest.7fb9ff5502777965d070e0c62a62add5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "98a7e930396a0af9d588ec3a5f323c60",
+    "url": "/promissu-web/static/media/logo.98a7e930.png"
+  },
+  {
+    "revision": "e02bc30d0a62e350ba1daec0bdcecfe4",
+    "url": "/promissu-web/static/media/button_download_ios.e02bc30d.png"
+  },
+  {
+    "revision": "77efec49589042d38d55c6148b1417bf",
+    "url": "/promissu-web/static/media/button_download_android.77efec49.png"
+  },
+  {
+    "revision": "8032aba1183029e6f261c3e2c871b3d8",
+    "url": "/promissu-web/static/media/05.8032aba1.png"
+  },
+  {
+    "revision": "6742f5c0af11dc9c4492b6690cf531ab",
+    "url": "/promissu-web/static/media/04.6742f5c0.png"
+  },
+  {
+    "revision": "733a8e6a3835918a8e2c",
+    "url": "/promissu-web/static/js/runtime~main.733a8e6a.js"
+  },
+  {
+    "revision": "4d83c01614b0d2478a99",
+    "url": "/promissu-web/static/js/main.4d83c016.chunk.js"
+  },
+  {
+    "revision": "da720ecd6ec00c6af248",
+    "url": "/promissu-web/static/js/1.da720ecd.chunk.js"
+  },
+  {
+    "revision": "4d83c01614b0d2478a99",
+    "url": "/promissu-web/static/css/main.143863e2.chunk.css"
+  },
+  {
+    "revision": "7d3bff80aa818c9221b00e525e6f91fc",
+    "url": "/promissu-web/index.html"
+  }
+];
